f5cbbef4c519da68cac2298089069c0ac05e9d82
Christian Laboranowitsch <christian.laboranowitsch@extern.cognizant-mobility.com>
2025-04-13 15:21:24 +0200

    Initial version

---
+++ src/test/kotlin/de/laboranowitsch/poc/grpcmock/GrpcMockApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.laboranowitsch.poc.grpcmock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrpcMockApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
